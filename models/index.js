e1c5063adb06660d4a239053e7025d8bd276448d
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-11-02 18:25:59 -0400

    Chnages to models and index js

--- models/index.js
+++ models/index.js
@@ -9,6 +9,32 @@ User.hasMany(Celebrity, {
 
 Celebrity.belongsTo(User, {
     foreignKey: "user_id",
-    onDelete: "CASCADE",
   });
 
+Celebrity.belongsTo(Fame, {
+    foreignKey: "fame_id",
+  });
+
+Fame.hasMany(Celebrity, {
+    foreignKey: "fame_id",
+  });
+
+Showdown.belongsTo(Celebrity, {
+    as: "attacker",
+    foreignKey: "attacker_id",
+  });
+
+Celebrity.hasMany(Showdown, {
+    as: "attacks",
+    foreignKey: "attacker_id",
+  });
+
+Showdown.belongsTo(Celebrity, {
+    as: "defender",
+    foreignKey: "defender_id",
+  });
+
+Celebrity.hasMany(Showdown, {
+    as: "defends",
+    foreignKey: "defender_id",
+  });
