12762c0c45dfce3dfe7a61ff8fb5b341fcc81c28
Vince <starryblue7@gmail.com>
2021-11-02 18:45:40 -0700

    routes tested successfully

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const celebrityRoutes = require('./celebrityRoutes');
+const fameRoutes = require('./fameRoutes');
+const showdownRoutes = require('./showdownRoutes');
 
 router.use('/users', userRoutes);
-router.use('/users', celebrityRoutes);
+router.use('/celebrities', celebrityRoutes);
+router.use('/fame', fameRoutes);
+router.use('/showdown', showdownRoutes);
 
 module.exports = router;
