b0e4e2662debcd1bdef99c4df27bfbc4be79d2a6
Vince <starryblue7@gmail.com>
2021-11-03 14:06:47 -0700

    add showdown route functionality

--- controllers/api/showdownRoutes.js
+++ controllers/api/showdownRoutes.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 const { Showdown } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
-    res.json('Add showdown results to history')
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newShowdown = await Showdown.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newShowdown);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 
