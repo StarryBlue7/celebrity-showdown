b62494a0d0762c5ba8f55d0e87a8fd398b9c0ab1
Vince <starryblue7@gmail.com>
2021-11-02 17:08:55 -0700

    set up home routes to render when handlebars files are ready

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,23 +5,35 @@ const sequelize = require('../config/connection');
 
 router.get('/', async (req, res) => {
     res.json('Homepage');
+    // res.render('homepage')
 });
 
-router.get('/showdown', async (req, res) => {
+router.get('/showdowns', async (req, res) => {
     res.json('Showdown page');
+    // res.render('showdowns')
 });
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/leaderboard', async (req, res) => {
+    res.json('Leaderboard page');
+    // res.render('leaderboard')
+});
+
+// router.get('/user/:id', async (req, res) => {
+//     res.json('See user page by id');
+// });
+
+router.get('/profile', async (req, res) => {
     res.json('User profile page');
+    // res.render('profile')
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/profile');
-        return;
-    }
-
-    res.render('login');
+    // if (req.session.logged_in) {
+    //     res.redirect('/profile');
+    //     return;
+    // }
+    res.json('login page');
+    // res.render('login');
 });
 
 module.exports = router;
