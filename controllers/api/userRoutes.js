78fee6eb394315764db9a94360a86931ea4ef364
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-03 16:26:05 -0700

    Added js script tags to main.handlebars and login/handlebars to support login/logout/signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Create a new User
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
-
         req.session.save(() => {
             req.session.user_id = userData.id;
-            req.session.user_name = userData.name;
+            req.session.user_name = userData.username;
             req.session.logged_in = true;
 
             res.status(200).json(userData);
@@ -17,20 +17,25 @@ router.post('/', async (req, res) => {
     }
 });
 
+// User Login
 router.post('/login', async (req, res) => {
     try {
+        // Locate record in User table using the email value
         const userData = await User.findOne({ where: { email: req.body.email } });
+        // If no matching record in user table, throw error
         if (!userData) {
             res.status(400).json({ message: 'Incorrect username or password, please try again' });
             return;
         }
-
+        // Compare password input value to password value in table record
         const validPassword = await userData.checkPassword(req.body.password);
+        // password input doesn't match value in table, throw error
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect username or password, please try again' });
             return;
         }
 
+        // create a session for this user
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.user_name = userData.name;
@@ -43,6 +48,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
+// User logout
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,14 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
+    console.log("Login user attempted");
+
     // Retrieve user values from the login form
     const email = document.querySelector('#email-login').value.trim();
+    
     const password = document.querySelector('#password-login').value.trim();
+
+    console.log(password);
     // if email and password values are provided, then authenticate and create session
     if (email && password) {
         const response = await fetch ('/api/users/login', {
@@ -13,9 +18,15 @@ const loginFormHandler = async (event) => {
         });
         // Once logged in, redirect the user to the profile page
         if (response.ok) {
+            console.log("Verified credentials");
             document.location.replace('/profile');
         } else {
+            console.log("Couldn't Verify credentials");
             alert(response.statusText)
         }
     }
 };
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler)
