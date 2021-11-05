27ab28442d81953003e572385da672f5dd973b51
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-04 19:05:19 -0700

    Feat: assignJustifyClass changes main element classes in response to window size changes, improving main UI in smaller screens.

---
+++ public/js/resize.js
@@ -0,0 +1,15 @@
+function assignJustifyClass() {
+    if ($(window).width() < 1036) {
+        $('.toggle-justify').removeClass('justify-content-between').addClass('justify-content-center');
+    } else {
+        $('.toggle-justify').removeClass('justify-content-center').addClass('justify-content-between');
+    }
+}
+
+
+// Listen for window resize
+window
+    .addEventListener('resize', assignJustifyClass)
+
+// Initialize function (to evaluate window size)
+assignJustifyClass();
